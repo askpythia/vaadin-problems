c9a94aa89e91680558f961c2fd0606c5015f2760
Robert Schild <y088734@GGAT-007551.at.top.com>
2021-03-15 10:49:11 +0100

    FACEER-649 : Extend UI Testcase with buttons (expand/collapse)

--- vaadin.problems/src/main/java/com/generali/fipos/vaadin/problems/TreeGridScrolltoItem.java
+++ vaadin.problems/src/main/java/com/generali/fipos/vaadin/problems/TreeGridScrolltoItem.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.hierarchy.TreeData;
 import com.vaadin.flow.data.provider.hierarchy.TreeDataProvider;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 
 @SuppressWarnings("serial")
 public class TreeGridScrolltoItem<T> extends VerticalLayout {
@@ -26,40 +27,44 @@ public class TreeGridScrolltoItem<T> extends VerticalLayout {
 		List<MapNode> rootItems = MapNode.buildSample(30,3, x ->   x+1);
 		List<MapNode> rootItems1 = MapNode.buildSample(30,3, x ->   x+1);
 		tree.setDataProvider((DataProvider<MapNode, ?>)	new TreeDataProvider<>(new TreeData<MapNode>().addItems(rootItems, 	md -> md.getChildren())));
-		// tree.addColumn(getRenderer(null, null));
-		tree.addHierarchyColumn(item -> item.getName());
+		tree.addColumn(getRenderer());
+		// tree.addHierarchyColumn(item -> item.getName());
 		MapNode _node = new MapNode("<TEST>");
 		tree.getTreeData().addRootItems(_node);
 		tree.getTreeData().addRootItems(rootItems1);
-
-		tree.setPageSize(1000);
+		tree.addExpandListener(event -> {
+			System.out.println("Expanded Event fired"); 
+		});
+		tree.setPageSize(10000);
 		add(tree);
 		tree.expandRecursively(rootItems, 1);
-		Button button = new Button("scroll to Element <TEST>");
-		button.addClickListener(e-> {
+		Button button1 = new Button("scroll to Node <TEST>");
+		button1.addClickListener(e-> {
 			tree.deselect(_node);
 			int _index = tree.getDataCommunicator().getIndex(_node);
-			System.out.println("of last Node with index: "+_index); 
+			System.out.println("Scroll to Node with index: "+_index); 
 			tree.scrollToIndex(_index);
 			tree.select(_node);
 		});
-		add(button);
+		Button button2 = new Button("collapse all Nodes");
+		button2.addClickListener(e-> {
+			tree.collapseRecursively(rootItems, 10);
+		});
+		Button button3 = new Button("expand all Nodes");
+		button3.addClickListener(e-> {
+			tree.expandRecursively(rootItems, 10);
+		});
+
+		add(button1,button2,button3);
 	}
-	/*
-	private TemplateRenderer<MapNode> getRenderer(String iconStyle, String textStyle) {
+
+	private TemplateRenderer<MapNode> getRenderer() {
 		TemplateRenderer<MapNode> _r = TemplateRenderer
 				.<MapNode> of("<vaadin-grid-tree-toggle data-id$='[[item.id]]' leaf='[[item.leaf]]' expanded='{{expanded}}' level='[[level]]'></vaadin-grid-tree-toggle><gli-icon class='configicon' style$='[[item.iconStyle]]' srcfiles='[[item.icon]]'></gli-icon><span style$='[[item.textStyle]]'>[[item.name]]</span>")
 				.withProperty("id", item -> item.getId()) //			
 				.withProperty("leaf", item -> item.getChildren().isEmpty()) //
 				.withProperty("name", item -> item.toString());
-		
-		 		// TODO: set Icons & check, if styles are needed ...
-				_r.withProperty("icon", item -> "[rect," + new Random().nextInt(360) + ",1,10]^**");
-				_r.withProperty("iconStyle", item -> iconStyle); //
-				_r.withProperty("textStyle", item -> textStyle); //
-
 		return _r;
 	}
-	*/
 
 }
